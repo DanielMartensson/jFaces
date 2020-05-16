ba888cd1e845999a8fe73392b94b759000d0dd7e
danielmartensson <daniel.martensson100@outlook.com>
2020-05-16 21:12:20 +0200

    K-nearest

--- src/main/java/se/danielmartensson/fisherfaces/Knn.java
+++ src/main/java/se/danielmartensson/fisherfaces/Knn.java
@@ -11,20 +11,21 @@ import se.danielmartensson.fisherfaces.matlab.Sum;
 
 public class Knn {
 
-	/**
-	 * Try to run this double[][] p = {{0.5815037 , 0.2126437 , 0.2778553 ,
-	 * 0.9896006 , 0.7984958 , 0.7577199 , 0.0470290, 0.0615568, 0.3166059,
-	 * 0.4474530}, {0.8899696 , 0.9083204 , 0.8482830 , 0.7057446 , 0.6736160 ,
-	 * 0.7784272 , 0.6961607 , 0.4131566 , 0.0023143 , 0.2253922}, {0.4088286 ,
-	 * 0.4405119 , 0.7431652 , 0.3027775 , 0.5088486 , 0.3344635 , 0.0390168 ,
-	 * 0.8200820 , 0.7779813 , 0.4115481}}; double[] y1 = {1 , 1 , 2 , 2 , 3 , 3 , 4
-	 * , 4 , 5 , 5}; double[][] q = {{1},{1},{10}}; int k = 3; RealMatrix P =
-	 * MatrixUtils.createRealMatrix(p); RealMatrix Y =
-	 * MatrixUtils.createRowRealMatrix(y1); RealMatrix Q =
-	 * MatrixUtils.createRealMatrix(q);
+	/** This is k-nearest neighbor 
+	 * Try to run this double[][] p = {{0.5815037 , 0.2126437 , 0.2778553 , 0.9896006 , 0.7984958 , 0.7577199 , 0.0470290, 0.0615568, 0.3166059, 0.4474530},
+	 *  							   {0.8899696 , 0.9083204 , 0.8482830 , 0.7057446 , 0.6736160 , 0.7784272 , 0.6961607 , 0.4131566 , 0.0023143 , 0.2253922}, 
+	 *                                 {0.4088286 , 0.4405119 , 0.7431652 , 0.3027775 , 0.5088486 , 0.3344635 , 0.0390168 , 0.8200820 , 0.7779813 , 0.4115481}}; 
+	 * double[] y1 = {1 , 1 , 2 , 2 , 3 , 3 , 4, 4 , 5 , 5}; 
+	 * double[][] q = {{1},{1},{10}}; 
+	 * int k = 3; RealMatrix P = MatrixUtils.createRealMatrix(p); 
+	 * RealMatrix Y = MatrixUtils.createRowRealMatrix(y1); 
+	 * RealMatrix Q = MatrixUtils.createRealMatrix(q);
 	 * 
-	 * knn(P, y, Q, k) = 4, for k = 1 knn(P, y, Q, k) = 2, for k = 2 knn(P, y, Q, k)
-	 * = 2, for k = 3 knn(P, y, Q, k) = 2, for k = 4 knn(P, y, Q, k) = 1, for k = 5
+	 * knn(P, y, Q, k) = 4, for k = 1 
+	 * knn(P, y, Q, k) = 2, for k = 2 
+	 * knn(P, y, Q, k) = 2, for k = 3 
+	 * knn(P, y, Q, k) = 2, for k = 4 
+	 * knn(P, y, Q, k) = 1, for k = 5
 	 * 
 	 */
 	static public int knn(RealMatrix P, RealMatrix y, RealMatrix Q, int k) {
